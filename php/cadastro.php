88808ce2204bf7d5e0c4323fdddaf996e0c68ba8
azazel_admetus <gustavodsv7184@gmail.com>
2025-07-03 11:21:11 -0400

    melhorei a segurança do arquivo cadastro e adicionei tratametos de erro

--- php/cadastro.php
+++ php/cadastro.php
@@ -6,11 +6,18 @@ require_once "conn.php";
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     // começando atribuindo as variáveis aos inputs 
 
-    $user = $_POST['username']; //Relaciono entre [] o name dos inputs
-    $email = $_POST['email'];
+    $user = trim($_POST['username']); //Relaciono entre [] o name dos inputs
+    $email = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
     $pass = password_hash($_POST['password'], PASSWORD_DEFAULT); //uso criptografia para as senhas. Um tipo de criptografia que não dá para descriptografar
     //função !empty verifica se os campos não estão vazios
     if (!empty($user) && !empty($pass)){
+        $check = $conn->prepare("SELECT id FROM users WHERE email = :email");
+        $check->bindParam(":email", $email);
+        $check->execute();
+        if($check->rowCount){
+            header("Location:../html/login.html?error=email_exists");
+            exit();
+        }
         try{
             $stmt = $conn->prepare("INSERT INTO users (nome, email, senha) VALUES (:user, :email, :pass)");
             $stmt->bindParam(':user', $user);
@@ -20,10 +27,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
                 header("Location: ../html/home.php");
                 exit();
             }else {
-                echo "Erro ao cadastrar: " . $stmt->errorInfo()[2];
+                header("Location:../html/cadastro.html?error=fail");
+                exit();
             }
         } catch (PDOException $e) {
-            echo "Erro ao cadastrar: " . $e->getMessage();
+            error_log($e->getMessage());
+            header("Location:../html/cadastro.html?error=empty");
+            exit();
         
         }
       
